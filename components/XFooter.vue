5fe941a0d0c9b33c1f8a37bb834ff09180575f41
lateautumn02 <jjl2801069764@gmail.com>
2026-05-06 15:39:25 +0800

    feat：删除过期链接

--- components/XFooter.vue
+++ components/XFooter.vue
@@ -4,20 +4,16 @@ withDefaults(defineProps<{
 }>(), {
   version: '1.0',
 })
+
+const currentYear = new Date().getFullYear()
 </script>
 
 <template>
   <div class="footer max-w-[1080px] mx-auto flex gap-2 text-xs my-4 px-2 md:px-0 pb-4 text-gray-400 justify-between md:justify-normal relative">
-    <span class="">
-      @2024
+    <span>
+      @2024-{{ currentYear }}
     </span>
     <span>版本v{{ version }}</span>
-    <span class="mx-1">•</span>
-    <a target="_blank" href="https://m.mblog.club">极简朋友圈</a>
-    <span class="mx-1">•</span>
-    <a target="_blank" href="https://dev.mblog.club">极简博客</a>
-    <span class="mx-1">•</span>
-    <a target="_blank" href="https://discussion.mblog.club">极简论坛</a>
   </div>
 </template>
 
